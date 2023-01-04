7045ddc40b48b719bb10aef5f497cfc3d0410818
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2023-01-03 21:46:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2023 Frigyes Erdosi-Szucs
+Copyright (c) 2023 Frigyes Erdosi-Szucs, Wes Sleeman
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
